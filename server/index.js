381240309563b259cf3ee5c0624df05dcfc935de
joaocolem <105292741+joaocolem@users.noreply.github.com>
2023-07-16 20:29:46 -0300

    joao branch

--- server/index.js
+++ server/index.js
@@ -20,6 +20,39 @@ app.get("/api/user/:id", (req, res) => {
     })
 });
 
+app.get("/api/motorista/:id/historico", (req, res) => {
+    const motoristaId = req.params.id;
+    const query = `SELECT v.* FROM motorista m
+                 JOIN viagem v ON m.CNHmotorista = v.idMotorista
+                 WHERE m.CNHmotorista = "${motoristaId}"`;
+
+    bd.query(query, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.json(data);
+    });
+});
+
+
+app.get("/api/passageiro/555.555.555-55/historico", () => {
+
+    const query = `SELECT v.* FROM passageiro p
+                   JOIN realiza r ON p.CPF = r.CPF
+                   JOIN viagem v ON r.ViagemID = v.ViagemID
+                   WHERE p.CPF = '555.555.555-55'`;
+  
+    bd.query(query, [cpfPassageiro], function (err, result, fields) {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Erro ao obter histórico de viagens do passageiro" });
+      } else {
+        res.json(result);
+      }
+    });
+  });
+  
+
+
 
 
 
--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -1,122 +1,65 @@
-import React, { useState } from "react";
-import Navbar from "../components/Navbar/Index";
-import { useOutletContext } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
 import UserTable from "./UserTable";
 
 function Table() {
-  const [sidebarToggle] = useOutletContext();
-
-  const [loading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/api/passageiro/555.555.555-55/historico')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
 
   const dataHeader = [
     {
-      key: "name",
-      label: "Name",
+      key: "Data",
+      label: "Data",
     },
     {
-      key: "email",
-      label: "Email",
+      key: "Hora",
+      label: "Hora",
     },
     {
-      key: "username",
-      label: "Username",
+      key: "Destino",
+      label: "Destino",
     },
     {
-      key: "role",
-      label: "Role",
+      key: "Origem",
+      label: "Origem",
     },
     {
-      key: "action",
-      label: "Aksi",
-    },
+      key: "Preco",
+      label: "Preço",
+    }
   ];
 
   const handleDelete = () => {};
-  const data = [
-    {
-      id: 1,
-      name: "Indah Sari Devi",
-      email: "mamahdedeh34@gmail.com",
-      username: "indahsdev01",
-      roles: [{ name: "Admin" }, { name: "Writer" }],
-    },
-    {
-      id: 2,
-      name: "Mahindra Putra",
-      email: "maheend@gmail.com",
-      username: "maheeend01",
-      roles: [{ name: "Editor" }],
-    },
-    {
-      id: 3,
-      name: "Ujang Ilman",
-      email: "ujangil03@gmail.com",
-      username: "uujang44",
-      roles: [{ name: "Writer" }],
-    },
 
-    {
-      id: 4,
-      name: "Hadi Pradhana",
-      email: "hapra09@gmail.com",
-      username: "hapra09",
-      roles: [{ name: "Writer" }],
-    },
-    {
-      id: 1,
-      name: "Indah Sari Devi",
-      email: "mamahdedeh34@gmail.com",
-      username: "indahsdev01",
-      roles: [{ name: "Admin" }, { name: "Writer" }],
-    },
-    {
-      id: 2,
-      name: "Mahindra Putra",
-      email: "maheend@gmail.com",
-      username: "maheeend01",
-      roles: [{ name: "Editor" }],
-    },
-    {
-      id: 3,
-      name: "Ujang Ilman",
-      email: "ujangil03@gmail.com",
-      username: "uujang44",
-      roles: [{ name: "Writer" }],
-    },
-
-    {
-      id: 4,
-      name: "Hadi Pradhana",
-      email: "hapra09@gmail.com",
-      username: "hapra09",
-      roles: [{ name: "Writer" }],
-    },
-    {
-      id: 4,
-      name: "Hadi Pradhana",
-      email: "hapra09@gmail.com",
-      username: "hapra09",
-      roles: [{ name: "Writer" }],
-    },
-  ];
   return (
-    <>
-      <main className="h-full">
-        <Navbar toggle={sidebarToggle} />
+    <div className="h-full">
+      <Navbar />
 
-        {/* Main Content */}
-        <div className="mainCard">
-          <div className="border w-full border-gray-200 bg-white py-4 px-6 rounded-md">
-            <UserTable
-              loading={loading}
-              dataHeader={dataHeader}
-              data={data}
-              handleDelete={handleDelete}
-            />
-          </div>
+      {/* Main Content */}
+      <div className="mainCard">
+        <div className="border w-full border-gray-200 bg-white py-4 px-6 rounded-md">
+          <UserTable
+            loading={loading}
+            dataHeader={dataHeader}
+            data={data}
+            handleDelete={handleDelete}
+          />
         </div>
-      </main>
-    </>
+      </div>
+    </div>
   );
 }
 
