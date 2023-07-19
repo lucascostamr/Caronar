efedb2d95940be52e56ecad02ecbe5158889e54e
joaocolem <105292741+joaocolem@users.noreply.github.com>
2023-07-19 15:19:32 -0300

    a

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ app.use(cors({
 
 app.get("/api/user/:id", (req, res) => {
   var userId = req.params.id;
-  var query = `SELECT * FROM bdcarona.passageiro AS user WHERE user.CPF = "${userId}"`;
-  console.log(query);
+  var query = "SELECT * FROM bdcarona.motorista AS user WHERE user.CNHmotorista = ?";
+
   bd.query(query, [userId], function (err, result, fields) {
     if (err) throw err;
     console.log(result);
@@ -27,7 +27,7 @@ app.get("/api/user/:id/historico", (req, res) => {
     FROM bdcarona.viagem AS v
     JOIN bdcarona.realiza AS r ON r.ViagemID = v.ViagemID
     JOIN bdcarona.motorista AS m ON v.idMotorista = m.CNHmotorista
-    WHERE r.CPF = "${userId}"
+    WHERE r.CPF = ?
   `;
 
   bd.query(query, [userId], function (err, result, fields) {
@@ -149,31 +149,17 @@ app.get("/api/melhores-motoristas", (req, res) => {
   });
 });
 
-app.get("/api/melhores-passageiros", (req, res) => {
-  var query = '';
 
-  bd.query(query, function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    res.json(result);
-  });
-})
 
-app.get("/api/passageiro/:cpf/imagem-perfil", (req, res) => {
-  var cnh = req.params.cnh;
 
-  var query = `
-    SELECT nome, classificacao, ImagemPerfil
-    FROM bdcarona.passageiro as passageiro
-    WHERE passageiro.CPF = ?
-  `;
 
-  bd.query(query, [cnh], function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    res.json(result);
-  });
-});
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
